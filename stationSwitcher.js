9efa1af80ec3fe31b706734b6c1872a8f5fb975d
Mel Harvey <mh89750@uw.edu>
2025-02-11 22:00:21 -0800

    RiceStation and Buttons

--- stationSwitcher.js
+++ stationSwitcher.js
@@ -4,10 +4,10 @@ export default class StationSwitcher {
     constructor(game, loadStation) {
         Object.assign(this, { game });
 
-        this.orderButton = new Button(game, 102, 636, "./assets/button/Order_Button.JPG", () => loadStation("order"));
-        this.riceButton = new Button(game, 318, 636, "./assets/button/Rice_Button.JPG", () => loadStation("burn"));
-        this.rollButton = new Button(game, 534, 636, "./assets/button/Roll_Button.JPG", () => loadStation("roll"));
-        this.sidesButton = new Button(game, 750, 636, "./assets/button/Side_Button.JPG", () => loadStation("sides"));
+        this.orderButton = new Button(game, 102, 694, "./assets/button/Order_Button.JPG", () => loadStation("order"));
+        this.riceButton = new Button(game, 318, 694, "./assets/button/Rice_Button.JPG", () => loadStation("burn"));
+        this.rollButton = new Button(game, 534, 694, "./assets/button/Roll_Button.JPG", () => loadStation("roll"));
+        this.sidesButton = new Button(game, 750, 694, "./assets/button/Side_Button.JPG", () => loadStation("sides"));
 
         game.addEntity(this.orderButton);
         game.addEntity(this.riceButton);
